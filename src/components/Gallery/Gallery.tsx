022fa8f4d6c7c98eb6e1bf05ad9f6d4d900b83a9
Josephmario247 <bartholomewjoseph81@gmail.com>
2024-04-10 16:12:22 +0100

    My final build Hotel-Management website with nextJs, typscript, sanityIo, stripe

---
+++ src/components/Gallery/Gallery.tsx
@@ -0,0 +1,71 @@
+
+import Image from 'next/image';
+
+const Gallery = () => {
+  return (
+    <div className='mx-auto container py-14 h-full'>
+      <div className='flex flex-wrap md:-m-2'>
+        <div className='flex w-1/2 flex-wrap'>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/hero_1.jpeg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/hero_2.jpeg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-full p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/hero3.jpeg'
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+        <div className='flex w-1/2 flex-wrap'>
+          <div className='w-full p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/hero1.jpeg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/hero2.jpeg'
+              width={200}
+              height={200}
+            />
+          </div>
+          <div className='w-1/2 p-1 md:p-2 h-48'>
+            <Image
+              alt='gallery'
+              className='img'
+              src='/images/hero_2.jpeg'
+              width={200}
+              height={200}
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Gallery;
